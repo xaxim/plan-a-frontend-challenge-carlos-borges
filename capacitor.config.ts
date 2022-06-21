d5f32e6af6173776c973586369ff141596533194
Carlos Lima <carloslima@mpdft.mp.br>
2022-06-21 11:36:05 -0300

    Starting the challenge

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plan-a-frontend-challenge-carlos-borges',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
